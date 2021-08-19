0e9564dacc2e1b3191a8c70edcaa833489802a49
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-19 13:27:54 -0700

    first function working

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,11 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    const total = a + b;
+    const answerArray = [];
+    answerArray.push (total);
+    answerArray.push(`The sum of ${a} and ${b} is ${total}.`);
+    return answerArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
